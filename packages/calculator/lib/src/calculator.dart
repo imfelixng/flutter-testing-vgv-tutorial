5b640b08e90e428343355d63e56ca0731f71f15f
An Nguyen Q <ngquangan.dev@gmail.com>
2022-06-27 00:13:07 +0700

    feat: apply matcher && add some test cases

--- packages/calculator/lib/src/calculator.dart
+++ packages/calculator/lib/src/calculator.dart
@@ -2,7 +2,7 @@
 class Calculator {
   double add(double a, double b) => a + b;
 
-  double substract(double a, double b) => a - b;
+  double subtract(double a, double b) => a - b;
 
   double multiply(double a, double b) => a * b;
 
