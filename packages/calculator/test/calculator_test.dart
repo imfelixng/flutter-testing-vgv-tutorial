113e04305ecee4dcb8b03d05cbacc8f52664d6f1
An Nguyen Q <ngquangan.dev@gmail.com>
2022-06-27 00:26:00 +0700

    refactor: add setup function to sharing calculator instance

--- packages/calculator/test/calculator_test.dart
+++ packages/calculator/test/calculator_test.dart
@@ -2,11 +2,17 @@ import 'package:calculator/calculator.dart';
 import 'package:test/test.dart';
 
 void main() {
+  late Calculator calculator;
+
+  setUp(() {
+    calculator = Calculator();
+  });
+
   group('init', () {
     test(
       'Calculator creates a non null object',
       () {
-        expect(Calculator(), isNotNull);
+        expect(calculator, isNotNull);
       },
     );
   });
@@ -15,7 +21,6 @@ void main() {
     test(
       'the calculator returns 4 when adding 2 and 2',
       () {
-        final calculator = Calculator();
         final result = calculator.add(2, 2);
         expect(result, 4.0, reason: 'It should be exactly 4');
       },
@@ -24,7 +29,6 @@ void main() {
     test(
       'the calculator returns 6 when adding 3 and 3',
       () {
-        final calculator = Calculator();
         final result = calculator.add(3, 3);
         expect(result, 6.0, reason: 'It should be exactly 6');
       },
@@ -35,7 +39,6 @@ void main() {
     test(
       'the calculator returns 5 when subtracting 7 and 2',
       () {
-        final calculator = Calculator();
         final result = calculator.subtract(7, 2);
         expect(result, 5.0, reason: 'It should be exactly 5');
       },
@@ -44,7 +47,6 @@ void main() {
     test(
       'the calculator returns 4 when subtracting 7 and 3',
       () {
-        final calculator = Calculator();
         final result = calculator.subtract(7, 3);
         expect(result, 4.0, reason: 'It should be exactly 4');
       },
@@ -55,7 +57,6 @@ void main() {
     test(
       'the calculator returns 16 when multiplying 8 and 2',
       () {
-        final calculator = Calculator();
         final result = calculator.multiply(8, 2);
         expect(result, 16.0, reason: 'It should be exactly 16');
       },
@@ -64,7 +65,6 @@ void main() {
     test(
       'the calculator returns 12 when multiplying 6 and 2',
       () {
-        final calculator = Calculator();
         final result = calculator.multiply(6, 2);
         expect(result, 12.0, reason: 'It should be exactly 12');
       },
@@ -75,7 +75,6 @@ void main() {
     test(
       'the calculator returns 4 when dividing 8 and 2',
       () {
-        final calculator = Calculator();
         final result = calculator.divide(8, 2);
         expect(result, 4.0, reason: 'It should be exactly 4');
       },
@@ -84,7 +83,6 @@ void main() {
     test(
       'the calculator returns 5 when dividing 10 and 2',
       () {
-        final calculator = Calculator();
         final result = calculator.divide(10, 2);
         expect(result, 5.0, reason: 'It should be exactly 5');
       },
@@ -93,7 +91,6 @@ void main() {
     test(
       'the calculator throws an ArgumentError when dividing by zero',
       () {
-        final calculator = Calculator();
         expect(() => calculator.divide(10, 0), throwsArgumentError);
       },
     );
